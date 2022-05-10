3d919f090c5941bccadaeeffef8bea31ab431558
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-10 13:58:01 -0400

    fixed Tailwind init

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div>
-            <header className="text-">
+            <header className="text-7xl">
                 <div>
                     UBC MC
                 </div>
