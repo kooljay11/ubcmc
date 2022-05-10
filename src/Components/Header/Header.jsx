f3cb7d7d4d82fe4c4913d5f0baa30a85ff296fa8
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-10 14:26:28 -0400

    added header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import UBCLogoWhite from './assets/ubc-logo-2018-crest-white-rgb72.png';
+
 export default function Header() {
     return (
         <div>
-            <header className="text-7xl">
+            <header className="text-3xl bg-color-1 text-white flex p-4">
                 <div>
+                    <img className='h-10' alt='ubc logo' src={UBCLogoWhite} />
+                </div>
+                <div className='bg-white w-1 mx-4'>
+
+                </div>
+                <div className='my-auto'>
                     UBC MC
                 </div>
             </header>
