4d7915b7762a15f0cdf0eb953f43e63604e29e13
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-10 14:25:36 -0400

    added ubc color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      "black": "#000000",
+      "white": "#FFFFFF",
+      "color-1": "#002145",
+      "color-2": "#97D4E9",
+      "color-3": "#6EC4E8",
+      "color-4": "#40B4E5",
+    },
     extend: {},
   },
   plugins: [],
