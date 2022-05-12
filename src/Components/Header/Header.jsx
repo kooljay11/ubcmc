34257f6a7d1c8533c0983a283392b907c26c4be9
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-12 00:18:52 -0400

    Deleted UBC logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,9 @@ import UBCLogoWhite from './assets/ubc-logo-2018-crest-white-rgb72.png';
 export default function Header() {
     return (
         <div>
-            <header className="text-3xl bg-color-1 text-white flex p-4">
-                <div>
-                    <img className='h-10' alt='ubc logo' src={UBCLogoWhite} />
-                </div>
-                <div className='bg-white w-1 mx-4'>
-
-                </div>
+            <header className="text-3xl bg-color-1 text-white text-center p-4 ">
                 <div className='my-auto'>
-                    UBC MC
+                    UBC Minecraft
                 </div>
             </header>
         </div>
