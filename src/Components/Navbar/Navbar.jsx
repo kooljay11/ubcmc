3121e487ee2be8f9314513a902cbb763f605ecb8
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-14 12:06:42 -0400

    Added Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-color-2 text-white  p-2 font-jetbrains-mono text-lg font-semibold">
+            <div className="">
+
+                <ul className="list-none text-color-1 flex px-2">
+                    <li>
+                        <a className="px-4" href="#">| Home |</a>
+                    </li>
+                    <li>
+                        <a className="px-4" href="#">| About Us |</a>
+                    </li>
+                    <li>
+                        <a className="px-4" href="#">| Factions SMP |</a>
+                    </li>
+                    <li>
+                        <a className="px-4" href="#">| Modded SMP |</a>
+                    </li>
+                    <li>
+                        <a className="px-4" href="#">| FAQ |</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    )
+} 
