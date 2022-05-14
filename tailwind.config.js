d718b6b422781dfd451795f6f81764c35e5ace51
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-14 12:05:31 -0400

    added custom fonts & changed color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ module.exports = {
     colors: {
       "black": "#000000",
       "white": "#FFFFFF",
-      "color-1": "#002145",
-      "color-2": "#97D4E9",
-      "color-3": "#6EC4E8",
-      "color-4": "#40B4E5",
+      "color-1": "#34572A",
+      "color-2": "#B3D575",
+    },
+    fontFamily: {
+      "bungee": ["Bungee", "sans-serif"],
+      "jetbrains-mono": ["JetBrainsMono", "sans-serif"]
     },
     extend: {},
   },
