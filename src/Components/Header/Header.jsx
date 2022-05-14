84b1cdbe20f9a3510478b8d8e80120a495a46ee1
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-05-14 12:07:09 -0400

    added custom fonts

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
         <div>
-            <header className="text-3xl bg-color-1 text-white text-center p-4 ">
-                <div className='my-auto'>
+            <header className="text-6xl bg-color-1 text-white text-center p-6">
+                <div className='my-auto font-bungee'>
                     UBC Minecraft
                 </div>
             </header>
